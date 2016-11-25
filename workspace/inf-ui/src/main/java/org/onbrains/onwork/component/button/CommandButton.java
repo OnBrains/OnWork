b4e23c18fe8fe1180ec896dd137a940b7feb6793
Oleg Naumov <o.naumov@argus>
2016-11-25 23:19:33 +0300

    #12 Разработать собственные компоненты

---
+++ workspace/inf-ui/src/main/java/org/onbrains/onwork/component/button/CommandButton.java
@@ -0,0 +1,20 @@
+package org.onbrains.onwork.component.button;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+
+/**
+ * Created by ol.naumov on 25.11.2016.
+ */
+@FacesComponent(value = "commandButton")
+public class CommandButton extends UINamingContainer {
+
+	// *****************************************************************************************************************
+	// Private methods
+	// *****************************************************************************************************************
+
+	// *****************************************************************************************************************
+	// Simple getters and setters
+	// *****************************************************************************************************************
+
+}
