38f64268840790d634ee81a738ae57563acb7fbc
Oleg Naumov <o.naumov@onbrains.org>
2016-11-28 13:34:31 +0300

    #12 Разработать собственные компоненты

---
+++ workspace/inf-ui/src/main/java/org/onbrains/onwork/component/CommandLink.java
@@ -0,0 +1,13 @@
+package org.onbrains.onwork.component;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+
+/**
+ * Created on 28.11.2016 2:19.
+ *
+ * @author Oleg Naumov
+ */
+@FacesComponent(value = "commandLink")
+public class CommandLink extends UINamingContainer {
+}
