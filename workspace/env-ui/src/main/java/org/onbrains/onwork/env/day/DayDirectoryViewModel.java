88464107d9dfb0638dd1efe619d07598dfb21c89
Oleg Naumov <o.naumov@onbrains.org>
2016-11-28 13:33:54 +0300

    #9 Реализовать справочник дней

--- workspace/env-ui/src/main/java/org/onbrains/onwork/env/day/DayDirectoryViewModel.java
+++ workspace/env-ui/src/main/java/org/onbrains/onwork/env/day/DayDirectoryViewModel.java
@@ -2,14 +2,28 @@ package org.onbrains.onwork.env.day;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.onbrains.onwork.env.day.model.Day;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.LazyScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
 
 /**
  * Created on 13.11.2016 20:49.
@@ -71,4 +85,13 @@ public class DayDirectoryViewModel implements Serializable {
 		return selectedMonth;
 	}
 
+	private List<Day> selectedDays;
+
+	public List<Day> getSelectedDays() {
+		return selectedDays;
+	}
+
+	public void setSelectedDays(List<Day> selectedDays) {
+		this.selectedDays = selectedDays;
+	}
 }
